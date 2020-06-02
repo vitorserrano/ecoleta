8786d0e109d55d6605fb38edd3a3da221d9d12a9
vitorserrano <vitorcirinoserrano@gmail.com>
2020-06-02 11:30:27 -0300

    ✨ Create table Items

--- server/src/database/migrations/01_create_items.ts
+++ server/src/database/migrations/01_create_items.ts
@@ -1,3 +1,13 @@
-export async function up() {}
+import Knex from 'knex';
 
-export async function down() {}
+export async function up(knex: Knex) {
+  return knex.schema.createTable('items', (table) => {
+    table.increments('id').primary();
+    table.string('image').notNullable();
+    table.string('title').notNullable();
+  });
+}
+
+export async function down(knex: Knex) {
+  return knex.schema.dropTable('items');
+}
