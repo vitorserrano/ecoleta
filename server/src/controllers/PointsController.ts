30cf6dedec3db8ae03f5b7e821f110246e23105d
vitorserrano <vitorcirinoserrano@gmail.com>
2020-06-02 16:39:14 -0300

    ✨ Add Index and Show for PointsController

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -2,6 +2,41 @@ import { Request, Response } from 'express';
 import knex from '../database/connection';
 
 class PointsController {
+  async index(request: Request, response: Response) {
+    const { city, uf, items } = request.query;
+
+    const parsedItems = String(items)
+      .split(',')
+      .map((item) => Number(item.trim()));
+
+    const points = await knex('points')
+      .join('point_items', 'points.id', '=', 'point_items.point_id')
+      .whereIn('point_items.item_id', parsedItems)
+      .where('city', String(city))
+      .where('uf', String(uf))
+      .distinct()
+      .select('points.*');
+
+    return response.json(points);
+  }
+
+  async show(request: Request, response: Response) {
+    const { id } = request.params;
+
+    const point = await knex('points').where('id', id).first();
+
+    if (!point) {
+      return response.status(400).json({ message: 'Point not found.' });
+    }
+
+    const items = await knex('items')
+      .join('point_items', 'items.id', '=', 'point_items.item_id')
+      .where('point_items.point_id', id)
+      .select('items.title');
+
+    return response.json({ point, items });
+  }
+
   async create(request: Request, response: Response) {
     const {
       name,
@@ -39,6 +74,8 @@ class PointsController {
     });
 
     await trx('point_items').insert(pointItems);
+
+    await trx.commit();
 
     return response.json({
       id: point_id,
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,24 +1,16 @@
 import express from 'express';
-import knex from '../src/database/connection';
+
 import PointsController from './controllers/PointsController';
+import ItemsController from './controllers/ItemsController';
 
 const routes = express.Router();
 const pointsController = new PointsController();
+const itemsController = new ItemsController();
 
-routes.get('/items', async (request, response) => {
-  const items = await knex('items').select('*');
-
-  const serializedItems = items.map((item) => {
-    return {
-      id: item.id,
-      title: item.title,
-      image_url: `http://localhost:3333/uploads/${item.image}`,
-    };
-  });
-
-  return response.json(serializedItems);
-});
+routes.get('/items', itemsController.index);
 
 routes.post('/points', pointsController.create);
+routes.get('/points/:id', pointsController.show);
+routes.get('/points', pointsController.index);
 
 export default routes;
