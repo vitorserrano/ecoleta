d81eb02aaf78474edd12571f29e53d00cc0d4124
vitorserrano <vitorcirinoserrano@gmail.com>
2020-06-02 11:34:20 -0300

    ✨ Create table point_items

--- server/src/database/migrations/02_create_point_items.ts
+++ server/src/database/migrations/02_create_point_items.ts
@@ -1,3 +1,14 @@
-export async function up() {}
+import Knex from 'knex';
 
-export async function down() {}
+export async function up(knex: Knex) {
+  return knex.schema.createTable('point_items', (table) => {
+    table.increments('id').primary();
+
+    table.integer('point_id').notNullable().references('id').inTable('points');
+    table.integer('item_id').notNullable().references('id').inTable('items');
+  });
+}
+
+export async function down(knex: Knex) {
+  return knex.schema.dropTable('point_items');
+}
