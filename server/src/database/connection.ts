c503e26e9b1d27753c239d7c04b530f6c9a42fa4
vitorserrano <vitorcirinoserrano@gmail.com>
2020-06-02 09:31:23 -0300

    ✨ Create table Points

--- server/src/database/connection.ts
+++ server/src/database/connection.ts
@@ -6,6 +6,7 @@ const connection = knex({
   connection: {
     filename: path.resolve(__dirname, 'database.sqlite'),
   },
+  useNullAsDefault: true,
 });
 
 export default connection;
---
+++ server/src/database/migrations/01_create_items.ts
@@ -0,0 +1,3 @@
+export async function up() {}
+
+export async function down() {}
