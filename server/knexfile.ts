73eb4cf8e26b965317554381abc9975dbc7b3761
vitorserrano <vitorcirinoserrano@gmail.com>
2020-06-02 11:50:20 -0300

    ✨ Add default seeds

--- server/knexfile.ts
+++ server/knexfile.ts
@@ -8,5 +8,8 @@ module.exports = {
   migrations: {
     directory: path.resolve(__dirname, 'src', 'database', 'migrations'),
   },
+  seeds: {
+    directory: path.resolve(__dirname, 'src', 'database', 'seeds'),
+  },
   useNullAsDefault: true,
 };
