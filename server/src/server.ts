9843982ed861037d650749ad1db81084b0ccc77b
vitorserrano <vitorcirinoserrano@gmail.com>
2020-06-02 16:48:22 -0300

    ✨ Add CORS

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,12 @@
 import path from 'path';
 
 import express from 'express';
+import cors from 'cors';
 import routes from './routes';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
