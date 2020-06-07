28476040ec6c8850b9fe95cf7042af91b840e68a
vitorserrano <vitorcirinoserrano@gmail.com>
2020-06-07 19:12:32 -0300

    ✨ Add Celebrate

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { celebrate, Joi } from 'celebrate';
 
 import multer from 'multer';
 import multerConfig from './config/multer';
@@ -15,8 +16,30 @@ const itemsController = new ItemsController();
 
 routes.get('/items', itemsController.index);
 
-routes.post('/points', upload.single('image'), pointsController.create);
 routes.get('/points/:id', pointsController.show);
 routes.get('/points', pointsController.index);
+
+routes.post(
+  '/points',
+  upload.single('image'),
+  celebrate(
+    {
+      body: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.number().required(),
+        latitude: Joi.number().required(),
+        longitude: Joi.number().required(),
+        city: Joi.string().required(),
+        uf: Joi.string().required(),
+        items: Joi.string().required(),
+      }),
+    },
+    {
+      abortEarly: false,
+    }
+  ),
+  pointsController.create
+);
 
 export default routes;
--- server/src/server.ts
+++ server/src/server.ts
@@ -2,7 +2,9 @@ import path from 'path';
 
 import express from 'express';
 import cors from 'cors';
+
 import routes from './routes';
+import { errors } from 'celebrate';
 
 const app = express();
 
@@ -11,5 +13,7 @@ app.use(express.json());
 app.use(routes);
 
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+
+app.use(errors());
 
 app.listen(3333);
